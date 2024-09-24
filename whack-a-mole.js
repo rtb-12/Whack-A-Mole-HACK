049a9764194927de6c26c8666f7b87a1c61967c6
Ronit <147048280+rtb-12@users.noreply.github.com>
2024-09-24 15:56:24 +0530

    HACK UPLOADED

---
+++ whack-a-mole.js
@@ -0,0 +1,27 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://nikkuag.github.io/Wack-A-Mole/");
+
+  await page.click("button");
+
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  async function clickMoles() {
+    while (true) {
+      const holesUp = await page.$$("div.hole.up");
+      for (let hole of holesUp) {
+        await hole.click();
+        console.log("Mole hit!");
+      }
+      await delay(50);
+    }
+  }
+
+  // Start clicking moles
+  clickMoles();
+})();
